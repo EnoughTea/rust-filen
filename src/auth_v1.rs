c2aa8ef9c2429f0cefbdc25012bd448c69edc0ff
Konstantin Zakharov <enough.tea@outlook.com>
2021-10-13 03:35:22 +0300

    Make two_factor_key mandatory for requests

--- src/auth_v1.rs
+++ src/auth_v1.rs
@@ -11,19 +11,24 @@ const LOGIN_PATH: &str = "/v1/login";
 #[skip_serializing_none]
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub struct AuthInfoRequestPayload {
+    /// Registered user email.
     pub email: String,
 
-    /// XXXXXX means no key
+    /// Registered user 2FA key, if present. XXXXXX means no 2FA key.
     #[serde(rename = "twoFactorKey")]
-    pub two_factor_key: Option<SecUtf8>,
+    pub two_factor_key: SecUtf8,
 }
 
+/// Response data for [AUTH_INFO_PATH] endpoint.
 #[skip_serializing_none]
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub struct AuthInfoResponseData {
+    /// Registered user email.
     pub email: String,
 
     /// Currently values of 1 & 2 can be encountered.
+    /// 1 means [FilenPasswordWithMasterKey::from_user_password] should be used to generate Filen password for login;
+    /// 2 means [FilenPasswordWithMasterKey::from_user_password_and_auth_info_salt] should be used instead.
     #[serde(rename = "authVersion")]
     pub auth_version: u32,
 
@@ -35,8 +40,13 @@ pub struct AuthInfoResponseData {
 #[skip_serializing_none]
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub struct AuthInfoResponsePayload {
+    /// True when API call was successful; false otherwise.
     pub status: bool,
+
+    /// Filen reason for success or failure.
     pub message: String,
+
+    /// Actual API call data.
     pub data: Option<AuthInfoResponseData>,
 }
 
@@ -44,29 +54,36 @@ pub struct AuthInfoResponsePayload {
 #[skip_serializing_none]
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub struct LoginRequestPayload {
+    /// Registered user email.
     pub email: String,
 
+    /// Filen-processed password. Note that this is not a registered user password, but its hash.
+    /// Use one of [FilenPasswordWithMasterKey]::from... methods to calculate it.
     pub password: SecUtf8,
 
-    /// XXXXXX means no key
+    /// Registered user 2FA key, if present. XXXXXX means no 2FA key.
     #[serde(rename = "twoFactorKey")]
-    pub two_factor_key: Option<SecUtf8>,
+    pub two_factor_key: SecUtf8,
 
     /// Currently values of 1 & 2 can be encountered.
+    /// Set to a value you received from auth/info call.
     #[serde(rename = "authVersion")]
     pub auth_version: u32,
 }
 
+/// Response data for [LOGIN_PATH] endpoint.
 #[skip_serializing_none]
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub struct LoginResponseData {
+    /// Filen API key.
     #[serde(rename = "apiKey")]
     pub api_key: SecUtf8,
 
-    /// This is plural, but seems to return just one key...
+    /// This is plural, but seems to return just one key... Used to decrypt various metadata.
     #[serde(rename = "masterKeys")]
     pub master_keys: SecUtf8,
 
+    /// For now it seems to be used only when decrypting shared download folder name.
     #[serde(rename = "privateKey")]
     pub private_key: SecUtf8,
 }
@@ -75,11 +92,17 @@ pub struct LoginResponseData {
 #[skip_serializing_none]
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub struct LoginResponsePayload {
+    /// True when API call was successful; false otherwise.
     pub status: bool,
+
+    /// Filen reason for success or failure.
     pub message: String,
+
+    /// Actual API call data.
     pub data: Option<LoginResponseData>,
 }
 
+/// Calls /v1/auth/info endpoint. Used to get used auth version and Filen salt.
 pub fn auth_info_request(
     payload: &AuthInfoRequestPayload,
     settings: &FilenSettings,
@@ -87,6 +110,7 @@ pub fn auth_info_request(
     utils::query_filen_api(AUTH_INFO_PATH, payload, settings)
 }
 
+/// Calls /v1/auth/info endpoint asynchronously. Used to get used auth version and Filen salt.
 pub async fn auth_info_request_async(
     payload: &AuthInfoRequestPayload,
     settings: &FilenSettings,
@@ -94,10 +118,12 @@ pub async fn auth_info_request_async(
     utils::query_filen_api_async(AUTH_INFO_PATH, payload, settings).await
 }
 
+/// Calls /v1/login endpoint. Used to get API key, master key(s?) and private key.
 pub fn login_request(payload: &LoginRequestPayload, settings: &FilenSettings) -> Result<LoginResponsePayload> {
     utils::query_filen_api(LOGIN_PATH, payload, settings)
 }
 
+/// Calls /v1/login endpoint asynchronously. Used to get API key, master key(s?) and private key.
 pub async fn login_request_async(
     payload: &LoginRequestPayload,
     settings: &FilenSettings,
@@ -119,7 +145,7 @@ mod tests {
         let (server, filen_settings) = init_server();
         let request_payload = AuthInfoRequestPayload {
             email: "test@email.com".to_owned(),
-            two_factor_key: None,
+            two_factor_key: SecUtf8::from("XXXXXX"),
         };
         let expected_response: AuthInfoResponsePayload =
             deserialize_from_file("tests/resources/responses/auth_info_v1.json");
@@ -143,7 +169,7 @@ mod tests {
         let (server, filen_settings) = init_server();
         let request_payload = AuthInfoRequestPayload {
             email: "test@email.com".to_owned(),
-            two_factor_key: None,
+            two_factor_key: SecUtf8::from("XXXXXX"),
         };
         let expected_response: AuthInfoResponsePayload =
             deserialize_from_file("tests/resources/responses/auth_info_v2.json");
@@ -168,7 +194,7 @@ mod tests {
         let request_payload = LoginRequestPayload {
             email: "test@email.com".to_owned(),
             password: SecUtf8::from("test"),
-            two_factor_key: Some(SecUtf8::from("XXXXXX")),
+            two_factor_key: SecUtf8::from("XXXXXX"),
             auth_version: 1,
         };
         let expected_response: LoginResponsePayload = deserialize_from_file("tests/resources/responses/login_v1.json");
