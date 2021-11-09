44c1116ad6ba8a2962ae594b1c44df9c3d4fe836
Konstantin Zakharov <enough.tea@outlook.com>
2021-11-10 02:24:48 +0300

    file/archive/restore API query

--- src/v1/versions.rs
+++ src/v1/versions.rs
@@ -7,13 +7,84 @@ use uuid::Uuid;
 
 type Result<T, E = Error> = std::result::Result<T, E>;
 
+const FILE_ARCHIVE_RESTORE_PATH: &str = "/v1/file/archive/restore";
 const FILE_VERSIONS_PATH: &str = "/v1/file/versions";
 
 #[derive(Snafu, Debug)]
 pub enum Error {
+    #[snafu(display("{} query failed: {}", FILE_ARCHIVE_RESTORE_PATH, source))]
+    FileArchiveRestoreQueryFailed {
+        payload: FileArchiveRestoreRequestPayload,
+        source: queries::Error,
+    },
+
     #[snafu(display("{} query failed: {}", FILE_VERSIONS_PATH, source))]
     FileVersionsQueryFailed { uuid: Uuid, source: queries::Error },
 }
+
+/// Used for requests to [FILE_ARCHIVE_RESTORE_PATH] endpoint.
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+pub struct FileArchiveRestoreRequestPayload {
+    /// User-associated Filen API key.
+    #[serde(rename = "apiKey")]
+    pub api_key: SecUtf8,
+
+    /// Archived file ID; hyphenated lowercased UUID V4.
+    pub uuid: Uuid,
+
+    /// Current file ID; hyphenated lowercased UUID V4.
+    #[serde(rename = "currentUUID")]
+    pub current_uuid: Uuid,
+}
+utils::display_from_json!(FileArchiveRestoreRequestPayload);
+
+/// Response data for [FILE_ARCHIVE_RESTORE_PATH] endpoint.
+#[skip_serializing_none]
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+pub struct FileArchiveRestoreResponseData {
+    /// Archived file ID; hyphenated lowercased UUID V4.
+    pub uuid: Uuid,
+
+    /// Current file ID; hyphenated lowercased UUID V4.
+    #[serde(rename = "currentUUID")]
+    pub current_uuid: Uuid,
+
+    /// File metadata.
+    pub metadata: String,
+
+    /// Name of the Filen bucket where file data is stored.
+    pub bucket: String,
+
+    /// Name of the Filen region where file data is stored.
+    pub region: String,
+
+    /// Amount of chunks file is split into.
+    pub chunks: u32,
+
+    /// Parent folder ID; hyphenated lowercased UUID V4.
+    pub parent: Uuid,
+
+    /// Random alphanumeric string associated with the file. Used for versioning.
+    pub rm: String,
+
+    /// File creation time, as Unix timestamp in seconds.
+    pub timestamp: u64,
+
+    /// Determines how file bytes should be encrypted/decrypted.
+    /// File is encrypted using roughly the same algorithm as metadata encryption,
+    /// use [crypto::encrypt_file_data] and [crypto::decrypt_file_data] for the task.
+    pub version: u32,
+
+    /// true if user has marked file as favorite; false otherwise.
+    #[serde(deserialize_with = "bool_from_int", serialize_with = "bool_to_int")]
+    pub favorited: bool,
+}
+utils::display_from_json!(FileArchiveRestoreResponseData);
+
+api_response_struct!(
+    /// Response for [FILE_ARCHIVE_RESTORE_PATH] endpoint.
+    FileArchiveRestoreResponsePayload<Option<FileArchiveRestoreResponseData>>
+);
 
 /// File version info.
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
@@ -44,6 +115,7 @@ pub struct FileVersion {
     /// use [crypto::encrypt_file_data] and [crypto::decrypt_file_data] for the task.
     pub version: u32,
 }
+
 /// Used for requests to [FILE_VERSIONS_PATH] endpoint.
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub struct FileVersionsRequestPayload {
@@ -70,6 +142,31 @@ api_response_struct!(
     /// Response for [FILE_VERSIONS_PATH] endpoint.
     FileVersionsResponsePayload<Option<FileVersionsResponseData>>
 );
+
+/// Calls [FILE_ARCHIVE_RESTORE_PATH] endpoint. Used to get versions of the given file.
+pub fn file_archive_restore_request(
+    payload: &FileArchiveRestoreRequestPayload,
+    filen_settings: &FilenSettings,
+) -> Result<FileArchiveRestoreResponsePayload> {
+    queries::query_filen_api(FILE_ARCHIVE_RESTORE_PATH, payload, filen_settings).context(
+        FileArchiveRestoreQueryFailed {
+            payload: payload.clone(),
+        },
+    )
+}
+
+/// Calls [FILE_ARCHIVE_RESTORE_PATH] endpoint asynchronously. Used to get versions of the given file.
+#[cfg(feature = "async")]
+pub async fn file_archive_restore_request_async(
+    payload: &FileArchiveRestoreRequestPayload,
+    filen_settings: &FilenSettings,
+) -> Result<FileArchiveRestoreResponsePayload> {
+    queries::query_filen_api_async(FILE_ARCHIVE_RESTORE_PATH, payload, filen_settings)
+        .await
+        .context(FileArchiveRestoreQueryFailed {
+            payload: payload.clone(),
+        })
+}
 
 /// Calls [FILE_VERSIONS_PATH] endpoint. Used to get versions of the given file.
 pub fn file_versions_request(
@@ -99,6 +196,40 @@ mod tests {
 
     static API_KEY: Lazy<SecUtf8> =
         Lazy::new(|| SecUtf8::from("bYZmrwdVEbHJSqeA1RfnPtKiBcXzUpRdKGRkjw9m1o1eqSGP1s6DM11CDnklpFq6"));
+
+    #[test]
+    fn file_archive_restore_request_should_be_correctly_typed() {
+        let request_payload = FileArchiveRestoreRequestPayload {
+            api_key: API_KEY.clone(),
+            uuid: Uuid::parse_str("b5ec90d2-957c-4481-b211-08a68accd1b2").unwrap(),
+            current_uuid: Uuid::parse_str("0d9e14cd-69be-4f44-8390-b493eaba3468").unwrap(),
+        };
+        validate_contract(
+            FILE_ARCHIVE_RESTORE_PATH,
+            request_payload,
+            "tests/resources/responses/file_archive_restore.json",
+            |request_payload, filen_settings| file_archive_restore_request(&request_payload, &filen_settings),
+        );
+    }
+
+    #[cfg(feature = "async")]
+    #[tokio::test]
+    async fn file_archive_restore_request_async_should_be_correctly_typed() {
+        let request_payload = FileArchiveRestoreRequestPayload {
+            api_key: API_KEY.clone(),
+            uuid: Uuid::parse_str("b5ec90d2-957c-4481-b211-08a68accd1b2").unwrap(),
+            current_uuid: Uuid::parse_str("0d9e14cd-69be-4f44-8390-b493eaba3468").unwrap(),
+        };
+        validate_contract_async(
+            FILE_ARCHIVE_RESTORE_PATH,
+            request_payload,
+            "tests/resources/responses/file_archive_restore.json",
+            |request_payload, filen_settings| async move {
+                file_archive_restore_request_async(&request_payload, &filen_settings).await
+            },
+        )
+        .await;
+    }
 
     #[test]
     fn file_versions_request_should_be_correctly_typed() {
